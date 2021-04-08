310c8abf05107c51b3f308ac9e3302dda3e91868
thelemax <thelemax@gmail.com>
2021-04-08 21:36:07 +0200

    Update nabboot.py

--- nabboot/nabboot.py
+++ nabboot/nabboot.py
@@ -4,38 +4,33 @@
 # At boot, set leds to orange.
 # At shutdown, turn leds off.
 
-from rpi_ws281x import Adafruit_NeoPixel, Color
+from apa102_pi.driver import apa102
 import sys
 
 
 def set_leds(shutdown):
+    LED_MOSI = 10 
+    LED_SCLK = 11
+    LED_BRIGHTNESS = 31
     LED_COUNT = 5
-    LED_PIN = 13  # GPIO pin connected to the pixels (18 uses PWM!).
-    LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-    LED_DMA = 12  # DMA channel to use for generating signal (try 10)
-    LED_BRIGHTNESS = 200  # Set to 0 for darkest and 255 for brightest
-    LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
-    LED_CHANNEL = 1  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-    strip = Adafruit_NeoPixel(
-        LED_COUNT,
-        LED_PIN,
-        LED_FREQ_HZ,
-        LED_DMA,
-        LED_INVERT,
-        LED_BRIGHTNESS,
-        LED_CHANNEL,
+    strip = apa102.APA102(
+        num_led=LedsAPA102.LED_COUNT, 
+        mosi=LedsAPA102.LED_MOSI, 
+        sclk=LedsAPA102.LED_SCLK, 
+        order='rgb',
     )
+    
     # Intialize the library (must be called once before other functions).
     strip.begin()
 
     if shutdown:
-        color = Color(0, 0, 0)
+        color = (0 << 16) + (0 << 8) + (0)
     else:
-        color = Color(255, 0, 255)
+        color = (255 << 16) + (0 << 8) + (255)
 
     for led in range(6):
-        strip.setPixelColor(led, color)
+        strip.set_pixel_rgb(led, color)
 
     strip.show()
 
